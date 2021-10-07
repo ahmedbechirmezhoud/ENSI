f6e0066cac99e390441c81f8a64aeaa51d7f9124
ahmedbechirmezhoud <79605129+ahmedbechirmezhoud@users.noreply.github.com>
2021-10-07 17:11:19 +0100

    Add Log capability too the app

---
+++ app/src/main/java/com/example/ensi/Logger.java
@@ -0,0 +1,67 @@
+package com.example.ensi;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Logger {
+
+
+    public static void log(String data){
+        writeToFile(data, App.getAppContext());
+        TextView txtView = (TextView) ((Activity)App.getAppContext()).findViewById(R.id.textView);
+        txtView.append(data);
+    }
+
+
+    private static void writeToFile(String data,Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+
+    private String readFromFile(Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("config.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append("\n").append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+
+}
